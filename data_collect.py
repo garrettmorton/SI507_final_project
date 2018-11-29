dad49c0e5d4075717a9e734ec8118ffbef27ab3f
Garrett Morton <gemorton@umich.edu>
2018-11-29 13:20:07 -0500

    additions to data_collect.py

--- data_collect.py
+++ data_collect.py
@@ -1,4 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import sqlite3 as sqlite
 
+#for testing
+CACHE_FNAME = "html_cache.json"
+DB_NAME = "lego.sqlite"
+
+def scrape_lego():
+	pass
+
+def build_db():
+	pass

2f5d8148a8e49b1d50edd3a4c41d6421b114abfa
Garrett Morton <gemorton@umich.edu>
2018-12-02 01:23:07 -0500

    scrape_set_list complete, scrape_set_info complete

--- data_collect.py
+++ data_collect.py
@@ -27,16 +27,15 @@ except:
 #####################################
 
 class LegoSet():
-	def __init__(self, name, number, theme, price, pieces, age_low, age_high, rating, tags):
+	def __init__(self, name, number, price, pieces, age_low, age_high, tags, theme):
 		self.name = name
 		self.number = number
-		self.theme = theme
 		self.price = price
 		self.pieces = pieces
 		self.age_low = age_low
 		self.age_high = age_high
-		self.rating = rating
 		self.tags = tags
+		self.theme = theme
 
 def scrape_theme_list():
 	#scrape html of page listing themes
@@ -61,36 +60,70 @@ def scrape_theme_list():
 	return url_list #[url, url, url]
 
 def scrape_set_info(url):
-	pass
+	if url in CACHE_DICTION.keys():
+		html_chunk = CACHE_DICTION[url]
+		data_div = BeautifulSoup(html_chunk, 'html.parser')
+	else:
+		html = requests.get(url).text
+		soup = BeautifulSoup(html, 'html.parser')
+		data_div = soup.find("div", attrs={"data-test":"product-view__itemscope"})
+		html_chunk = str(data_div)
+		CACHE_DICTION[url] = html_chunk
+
+	name = data_div.find(class_="overview__name markup").get_text()
+	price = data_div.find(class_="product-price__list-price").get_text()[1:]
+	tags_list = []
+	tags_tags = data_div.find_all("a", class_="badges__tag")
+	for item in tags_tags:
+		tags_list.append(item.get_text())
+
+	number = data_div.find("dd", class_="product-details__product-code").get_text()
+	pieces = data_div.find("dd", class_="product-details__piece-count").get_text()
+	ages = data_div.find("dd", class_="product-details__ages").get_text()
+	age_tuple = ages.partition("-")
+	if age_tuple[1] == "":
+		age_low = ages[:-1]
+		age_high = None
+	else:
+		age_low = age_tuple[0]
+		age_high = age_tuple[2]
+
+	set_tuple = (name, number, price, pieces, age_low, age_high, tags_list)
+
+	return set_tuple # (name, number, price, pieces, age_low, age_high, [tag, tag, tag])
 
 def scrape_set_list(baseurl):
 	theme = baseurl.split("/")[-1]
 	theme = theme.replace("-", " ").capitalize()
+	set_objects = []
 
+	#scrape every page of paginated list
 	pagination = True
 	pager = 1
 	while pagination:
 		list_page_url = baseurl + "?page=" + str(pager)
 		if list_page_url in CACHE_DICTION.keys():
 			html_chunk = CACHE_DICTION[list_page_url]
-			set_ul = soup.find("ul", class_="ProductGridstyles__Grid-lc2zkx-2 dijnMv")
+			set_ul = BeautifulSoup(html_chunk, 'html.parser')
 		else:
 			html = requests.get(list_page_url).text
 			soup = BeautifulSoup(html, 'html.parser')
 			set_ul = soup.find_all("ul", class_="ProductGridstyles__Grid-lc2zkx-2 dijnMv")
-			if len(set_list) == 0:
+			if len(set_ul) == 0:
 				pagination = False
 				break
-			html_chunk = str(set_list[0])
+			html_chunk = str(set_ul[0])
 			CACHE_DICTION[list_page_url] = html_chunk
 
-		set_a_list = set_ul.find_all("a", class_="ProductImage__ProductImageLink-s1x2glqd-0 esZrQH")
-		set_url_list = []
-		for item in set_a_list:
-			set_url = "https://shop.lego.com" + item.["href"]
+		set_a_tags = set_ul.find_all("a", class_="ProductImage__ProductImageLink-s1x2glqd-0 esZrQH")
+		for item in set_a_tags:
+			set_url = "https://shop.lego.com" + item["href"]
+			set_tuple = scrape_set_info(set_url)
+			set_objects.append(LegoSet(*set_tuple, theme))
 
-			## DO SCRAPE SET INFO HERE
-	pass
+		pager += 1
+
+	return set_objects
 
 def scrape_all_data():
 	theme_url_list = scrape_theme_list()
@@ -104,7 +137,8 @@ def build_db():
 
 #####################################
 ######-------FOR TESTING-------######
-scrape_theme_list()
+#scrape_theme_list()
+print(scrape_set_info("https://shop.lego.com/en-US/Betrayal-at-Cloud-City-75222"))
 
 cache_file = open(CACHE_FNAME, 'w')
 cache_contents = json.dumps(CACHE_DICTION)
